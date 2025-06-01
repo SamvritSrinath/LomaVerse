37decb48addab17c3c492731032202382f295a20
Samvrit Srinath <sasrinath@ucsd.edu>
2025-06-01 07:17:39 -0700

    WIP

--- project/utils.py
+++ project/utils.py
@@ -1,6 +1,6 @@
 from config import BodyState, SolarSystemConfig
 
-def convert_ctype_state_to_body_state(ctype_state, cfg: SolarSystemConfig):
+def convert_ctype_state_to_body_state(ctype_state, cfg: SolarSystemConfig) -> list[BodyState]:
     body_configs = []
     for i in range(cfg.current_n_bodies):
         body_config = BodyState()
